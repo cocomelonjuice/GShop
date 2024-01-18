b26f49404111b210184ce6da8da0f13fafdd146c
cocomelonjuice <minhtiendh2018@gmail.com>
2024-01-18 22:53:16 +0700

    update Slice.jsx

--- src/components/Coffee.jsx
+++ src/components/Coffee.jsx
@@ -3,9 +3,7 @@ import Navbar from "./Navbar"
 
 
 export default function Coffee () {
-
   const listCoffee = (productList.filter(item=>item.type=="Coffee")).map(item=> <li>{item.name}</li>)
-  
   return (
     <div>
       <Navbar></Navbar>
--- src/features/Slice.jsx
+++ src/features/Slice.jsx
@@ -1,3 +1,44 @@
 import { createSlice } from "@reduxjs/toolkit"
 import { productList } from "../ProductData"
 
+const initialState = {
+    products: productList,
+    amount: 0,
+    total: 0
+}
+
+const cartSlice =  createSlice ({
+    name: "cart",
+    initialState,
+    reducers: {
+        increaseAmount: (state, {payload})=>{
+            const item = state.products.find (item=>item.name===payload.name);
+            item.amount++;
+        },
+
+        decreaseAmount: (state, {payload})=>{
+            const item = state.products.find (item=>item.name===payload.name);
+            item.amount--;
+        },
+
+        removeItem: (state, {payload})=>{
+            state.products=state.products.filter (item=>item.name!=payload.name)
+        },
+
+        updateTotal : (state)=>{
+            let amount = 0;
+            let total = 0;
+            state.products.forEach (item=>{
+                amount +=item.amount;
+                total+= item.amount*item.price
+            });
+            state.amount =  amount;
+            state.total = total;
+
+        }
+    }
+
+})
+
+export const {increaseAmount, decreaseAmount, removeItem, updateTotal} =cartSlice.actions;
+export default cartSlice.reducer;
