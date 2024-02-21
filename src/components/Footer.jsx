cae51dae3416e6e8879f881ea980290370116b7b
cocomelonjuice <minhtiendh2018@gmail.com>
2024-02-21 22:02:31 +0700

    remove #root padding style in App.css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { IoIosPhonePortrait } from "react-icons/io";
 
 export default function Footer() {
   return (
-    <div className="fixed bottom-0 left-0 right-0 flex flex-row justify-evenly bg-teal-300 text-black ">
+    <div className="bottom-0  m-auto  left-0 right-0 flex flex-row justify-evenly bg-teal-300 text-black ">
       <h1><FaFacebook /></h1>
       <h1><IoMdMail /></h1>
       <h1><IoIosPhonePortrait /></h1>
