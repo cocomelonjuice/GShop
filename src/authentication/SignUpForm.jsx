7a27155006fb39d51dd6985125203e0ec1ea9a6a
cocomelonjuice <minhtiendh2018@gmail.com>
2024-03-10 22:07:13 +0700

    update SignIn

--- src/authentication/SignUpForm.jsx
+++ src/authentication/SignUpForm.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { useSignup } from "./useSignup";
 
-//fix : react hook form error not showing
+//fix : react hook form error display only when click "Complete" button first, video 393
 
 function SignUpForm (){
-  const {register, formState, getValues, handleSubmit, formState: { errors },}= useForm();
+
+  const {signup, isLoading}= useSignup();
+
+  const {register, getValues, handleSubmit, formState: { errors },reset}= useForm();
   //const {errors}= formState;
 
-  function onSubmit(data){
-    console.log(data);
+  function onSubmit({fullName, email, password}){
+    signup({fullName, email, password})
   }
 
   return (
@@ -19,27 +23,28 @@ function SignUpForm (){
             CREATE NEW ACCOUNT
             </h1>
            
-            <div error={errors?.userName?.message}  className='flex flex-row m-auto gap-4'>
+            <div className='flex flex-row m-auto gap-4'>
                 <h1>Username</h1>
                 <input type="text" id="userName" {...register("userName", {required: "This field is required"})}  className='text-gray-600 italic border-gray-400 border-2 rounded-md '/>
+                <p className="text-red-500">{errors.userName?.message}</p>
             </div>
 
-            <div error={errors?.email?.message}  className='flex flex-row m-auto gap-5'>
+            <div className='flex flex-row m-auto gap-5'>
                 <h1>Email Address</h1>
                 <input type="email" id="email" {...register("email", {required: "This field is required", pattern:{ value: /\S+@\S+\.\S+/, message: "Enter valid email address"}})} className="text-gray-600 italic  border-gray-400 border-2  rounded-md" /> {/*regex email validation */}
+                <p className="text-red-500">{errors.email?.message}</p>
             </div>
 
-            <div   className='flex flex-row m-auto gap-5'>
+            <div className='flex flex-row m-auto gap-5'>
                 <h1>Password</h1>
                 <input type="password" id="password" {...register("password", {required: "This field is required", minLength: { value:8, message: "Minimum 8 characters"}})} className="text-gray-600 italic  border-gray-400 border-2  rounded-md" />
             <p className="text-red-500">{errors.password?.message}</p>
-
             </div>
  
-
-            <div error={errors?.passwordConfirm?.message}  className='flex flex-row m-auto gap-5'>
+            <div className='flex flex-row m-auto gap-5'>
                 <h1>Repeat Password</h1>
-                <input type="password"  id="passwordConfirm" {...register("passwordConfirm", {required: "This field is required",validate:(value)=>value===getValues().password||"Repeat your password" })} className="text-gray-600 italic  border-gray-400 border-2  rounded-md" />
+                <input type="password"  id="passwordConfirm" {...register("passwordConfirm", {required: "This field is required",validate:(value)=>value===getValues().password||"Passwords do not match" })} className="text-gray-600 italic  border-gray-400 border-2  rounded-md" />
+                <p className="text-red-500">{errors.passwordConfirm?.message}</p>
             </div>
           
             <button stype="submit" className='text-white bg-purple-500 max-w-fit m-auto p-1 rounded-md mt-4'>Complete</button>
