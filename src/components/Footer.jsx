87e4aa9ee91ea5ac69ab10ba4004707b022bc416
cocomelonjuice <minhtiendh2018@gmail.com>
2024-03-26 22:19:47 +0700

    update Footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,13 @@ import { IoIosPhonePortrait } from "react-icons/io";
 
 export default function Footer() {
   return (
-    <div className="bottom-0  m-auto  left-0 right-0 flex flex-row justify-evenly bg-teal-300 text-black ">
-      <h1><FaFacebook /></h1>
-      <h1><IoMdMail /></h1>
-      <h1><IoIosPhonePortrait /></h1>
+    <div className="bottom-0  m-auto  left-0 right-0 flex flex-col  bg-teal-300 text-black min-h-10 content-center items-center">
+      <div className="flex flex-row gap-20 justify-evenly">
+      <a href="https://www.facebook.com/juice.cocomelonjuice/"><FaFacebook /></a>
+      <a href="mailto:minhtiendh2018@gmail.com"><IoMdMail /></a>
+      <a href="tel:0765162701"><IoIosPhonePortrait /></a>
+      </div>
+      <h1>2024, ShopG.</h1>
     </div>
   )
 }
