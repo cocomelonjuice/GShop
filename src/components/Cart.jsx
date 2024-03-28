73513019a41d7ca3c837cc6c8d322c983616c138
cocomelonjuice <minhtiendh2018@gmail.com>
2024-03-28 22:57:55 +0700

    update Checkout page

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -6,6 +6,8 @@ import {store} from "../redux/ReduxStore"
 import { add,removeFromCart,decreaseCart, getTotals } from "../features/Slice.jsx";
 import { useDispatch } from "react-redux";
 
+import Checkout from "./Checkout.jsx";
+
 const Cart = ({item}) => {
 
   const {cart,choosenItems, cartTotalAmount}=useSelector((state)=>state.cart);
@@ -32,7 +34,7 @@ const Cart = ({item}) => {
               <div>Total Distinct Item: {choosenItems.length}</div>
               <div>Cart total quantity: {cartTotalQuantity}</div>
               <div>Sub Total Price:$ {cartTotalAmount}</div>
-              <button>CHECKOUT</button>
+              <button onClick={Checkout}>CHECKOUT</button>
         </div>
       ):(
         <div className="mt-20 text-black flex content-center justify-center">Your Cart is empty</div>
---
+++ src/components/Checkout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Cart from './Cart'
+
+const Checkout = () => {
+  return (
+    <div>Checkout</div>
+  )
+}
+
+export default Checkout
